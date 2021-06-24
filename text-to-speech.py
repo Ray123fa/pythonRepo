93fb76164b5768986e21b65e1112c601913e4ad5
Rayhan F <70044074+Ray123fa@users.noreply.github.com>
2021-06-24 15:18:21 +0800

    Create text-to-speech.py

---
+++ text-to-speech.py
@@ -0,0 +1,25 @@
+#created by rayhan.frdh
+#google text-to-speech adapted by python.hub
+
+from gtts import gTTS
+import random
+
+def main():
+  num = "1234567890"
+  char = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+  uniqid = "".join(random.sample(f"{num}{char}", 8))
+  
+  audio = f"TtS_{uniqid.lower()}.mp3"
+  convtext = input("Masukkan teks: \n")
+  setting = gTTS(text = convtext, lang = "id", slow = False)
+  
+  setting.save(audio)
+  print("\nBerhasil konversi teks ke audio")
+  
+  #kode meminta validasi user apakah ingin menjalankan ulang
+  rerun = input("Ulang program? (Y/N): ")
+  if rerun == "Y" or rerun == "y":
+    main()
+  if rerun == "N" or rerun == "n":
+    print("\nThank you for using this program.")
+main()
